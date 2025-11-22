f983220c1076776b1371616a819d18574ebdc26a
dramirezbe <dramirezbe@unal.edu.co>
2025-11-21 22:37:01 -0500

    Add i3, fix kitty conf alien and zsh-autosuggest

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,14 +1,7 @@
-#Paths
-KITTY_CFG=$HOME/.config/kitty/kitty.conf
-
-cd $HOME
-
-#create dirs
-mkdir -p $ZSH_CUSTOM/plugins
-mkdir -p $KITTY_CFG
-
 sudo apt update
-sudo apt install python3-full git cmake make build-essential curl wget zsh fonts-powerline kitty zsh-syntax-highlighting zsh-autocomplete
+
+sudo apt install python3-full git cmake make build-essential curl wget zsh fonts-powerline kitty zsh-syntax-highlighting fonts-jetbrains-mono nitrogen polybar
+sudo fc-cache -f -v
 
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -21,7 +14,6 @@ git submodule update --init --recursive --remote
 
 #install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 
 
 
